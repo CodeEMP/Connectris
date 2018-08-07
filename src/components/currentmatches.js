a91f835be9751733d59bcffabac0e1e2b755888b
CodeEMP <cpgameface@gmail.com>
2018-08-07 18:56:52 -0500

    routes

--- src/components/currentmatches.js
+++ src/components/currentmatches.js
@@ -8,6 +8,11 @@ class Matches extends Component {
         MATCH LIST PH
         <h1>MATCH LIST PH</h1>
         <Link to="/"><button>HOME</button></Link>
+        <div className="homemenu">
+          <Link to="/matches"><button className="homebutton">Games</button></Link>
+          <Link to="/matchmaking"><button className="homebutton">Find match</button></Link>
+          <Link to="/friends"><button className="homebutton">Friends</button></Link>
+        </div>
       </div>
     )
   }
--- src/components/matchmaking.js
+++ src/components/matchmaking.js
@@ -7,6 +7,11 @@ class Matchmaking extends Component {
       <div>
         <h1>Matchmaking PH</h1>
         <Link to="/"><button>HOME</button></Link>
+        <div className="homemenu">
+          <Link to="/matches"><button className="homebutton">Games</button></Link>
+          <Link to="/matchmaking"><button className="homebutton">Find match</button></Link>
+          <Link to="/friends"><button className="homebutton">Friends</button></Link>
+        </div>
       </div>
     )
   }
